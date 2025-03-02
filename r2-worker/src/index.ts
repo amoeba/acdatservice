644abb0341d47b9708d7ad5085b8aa2c1fa57a37
Bryce Mecum <petridish@gmail.com>
2025-03-01 18:03:07 -0800

    Get a basic range request working

--- r2-worker/src/index.ts
+++ r2-worker/src/index.ts
@@ -1,18 +1,30 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { R2Bucket } from '@cloudflare/workers-types';
+
+interface Env {
+	DATS_BUCKET: R2Bucket;
+}
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		const startByte = 0;
+		const numberOfBytes = 5;
+
+		const object = await env.DATS_BUCKET.get("portal.dat", {
+			range: {
+				offset: startByte,
+				length: numberOfBytes
+			}
+		});
+
+		if (object === null) {
+			return new Response("Object Not Found", { status: 404 });
+		}
+
+		const headers = new Headers();
+		// TODO
+		// headers.set("Content-Type", object.httpMetadata?.contentType);
+		headers.set("Content-Range", `bytes ${startByte}-${startByte + numberOfBytes - 1}/${object.size}`);
+
+		return new Response(object.body, { status: 206, headers });
 	},
 } satisfies ExportedHandler<Env>;

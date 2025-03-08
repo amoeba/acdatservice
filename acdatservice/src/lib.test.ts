fbaa25e9fdd63c08287fc5eebb789c1c28620b49
Bryce Mecum <petridish@gmail.com>
2025-03-08 09:04:32 -0800

    Write basic binaryreader tests

--- acdatservice/src/lib.test.ts
+++ acdatservice/src/lib.test.ts
@@ -2,24 +2,21 @@
 import BinaryReader from './binary_reader';
 import SeekableFileReader from './seekable_file_reader';
 
+const rdr = function (values: number[]): BinaryReader {
+  const buf = Buffer.from(values);
+  const array = new Uint8Array(buf);
+
+  return new BinaryReader(array.buffer)
+}
+
 test('BinaryReader smoke test', () => {
-  // WIP
-  let input = new Uint8Array(4);
-
-  input[0] = 0x00;
-  input[1] = 0x01;
-  input[2] = 0x02;
-  input[3] = 0x03;
-
-  let reader = new BinaryReader(input.buffer);
-  let result = reader.ReadUint32();
-
-  expect(result).toBe(4);
+  expect(rdr([0x01, 0x00, 0x00, 0x00]).ReadUint32()).toBe(1);
+  expect(rdr([0xFF, 0x00, 0x00, 0x00]).ReadUint32()).toBe(255);
+  expect(rdr([0x03, 0x02, 0x01, 0x00]).ReadUint32()).toBe(66051);
 });
-
 
 test('SeekableFileReader smoke test', () => {
   // TODO
 
-  expect(2).toBe(4);
+  // expect(2).toBe(4);
 });

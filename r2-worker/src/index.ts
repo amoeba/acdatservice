855e16676b45c248276e2617337b52c50a3efce7
Bryce Mecum <petridish@gmail.com>
2025-03-08 20:42:33 -0800

    hook photon up

--- r2-worker/src/index.ts
+++ r2-worker/src/index.ts
@@ -1,4 +1,5 @@
 import { R2Bucket } from '@cloudflare/workers-types';
+import { PhotonImage } from "@cf-wasm/photon";
 
 interface Env {
   DATS_BUCKET: R2Bucket;
@@ -9,28 +10,73 @@ const Objects = {
   "CELL_DAT": { "key": "cell.dat", "contentType": "application/octet-stream" }
 }
 
+const handle = async function (request, env, ctx): Promise<Response> {
+  let url = new URL(request.url);
+  // TODO: Appropraite error response ofr invalid request
+
+  let file_type = url.searchParams.get("type");
+  let id = url.searchParams.get("id");
+
+  // TODO: Error response for invalid input
+  const obj = Objects.PORTAL_DAT;
+
+  const startByte = 0;
+  const numberOfBytes = 5;
+
+  const object = await env.DATS_BUCKET.get(obj.key, {
+    range: {
+      offset: startByte,
+      length: numberOfBytes
+    }
+  });
+
+  // let buf = new Uint8Array(4096);
+
+  // for (let i = 0; i < buf.length; i++) {
+  // buf[i] = 0;
+  // }
+
+  const imageUrl = "https://avatars.githubusercontent.com/u/314135";
+  const inputBytes = await fetch(imageUrl)
+    .then((res) => res.arrayBuffer())
+    .then((buffer) => new Uint8Array(buffer));
+
+  const inputImage = PhotonImage.new_from_byteslice(inputBytes);
+  let out = inputImage.get_bytes_webp();
+  inputImage.free();
+
+
+  const headers = new Headers();
+  headers.set("Content-Type", "image/webp");
+
+
+  return new Response(out, { status: 200, headers });
+}
+
 export default {
   async fetch(request, env, ctx): Promise<Response> {
-    const obj = Objects.PORTAL_DAT;
+    return await handle(request, env, ctx);
 
-    const startByte = 0;
-    const numberOfBytes = 5;
+    // const obj = Objects.PORTAL_DAT;
 
-    const object = await env.DATS_BUCKET.get(obj.key, {
-      range: {
-        offset: startByte,
-        length: numberOfBytes
-      }
-    });
+    // const startByte = 0;
+    // const numberOfBytes = 5;
 
-    if (object === null) {
-      return new Response("Object Not Found", { status: 404 });
-    }
+    // const object = await env.DATS_BUCKET.get(obj.key, {
+    //   range: {
+    //     offset: startByte,
+    //     length: numberOfBytes
+    //   }
+    // });
 
-    const headers = new Headers();
-    headers.set("Content-Type", obj.contentType);
-    headers.set("Content-Range", `bytes ${startByte}-${startByte + numberOfBytes - 1}/${object.size}`);
+    // if (object === null) {
+    //   return new Response("Object Not Found", { status: 404 });
+    // }
 
-    return new Response(object.body, { status: 206, headers });
+    // const headers = new Headers();
+    // headers.set("Content-Type", obj.contentType);
+    // headers.set("Content-Range", `bytes ${startByte}-${startByte + numberOfBytes - 1}/${object.size}`);
+
+    // return new Response(object.body, { status: 206, headers });
   },
 } satisfies ExportedHandler<Env>;

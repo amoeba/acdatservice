c98add5a21370914cbe903f6355bc54765a56273
Bryce Mecum <petridish@gmail.com>
2025-03-08 17:08:42 -0800

    Update Texture.ts

--- acdatservice/src/dat/Texture.ts
+++ acdatservice/src/dat/Texture.ts
@@ -8,15 +8,14 @@ export class Texture {
   buffer: Buffer | undefined
 
   unpack(reader: SeekableFileReader) {
-    // TODO: What is this?
+    // ID?
     reader.ReadUint32();
+
     // TODO: What is this?
     reader.ReadUint32();
 
     // Form? What is form?
     let form = reader.ReadUint32();
-
-    console.log(form);
 
     if (form == 10 || form == 6) {
       this.width = reader.ReadUint32();
@@ -28,6 +27,7 @@ export class Texture {
     }
 
     this.buffer = Buffer.alloc(this.length);
+
     for (let i = 0; i < this.length; i++) {
       this.buffer[i] = reader.ReadUint8()
     }

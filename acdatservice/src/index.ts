ea479922d78188f25d634b3c41fe3570646f283d
Bryce Mecum <petridish@gmail.com>
2025-03-08 16:32:45 -0800

    Create PortalIcon class

--- acdatservice/src/index.ts
+++ acdatservice/src/index.ts
@@ -2,6 +2,7 @@ const fs = require('fs');
 
 import BinaryReader from "./binary_reader";
 import { DatDatabase } from "./lib";
+import { PortalIcon } from "./portal_icon";
 import SeekableFileReader from "./seekable_file_reader";
 import sharp from "sharp";
 
@@ -34,48 +35,19 @@ const main = function () {
 
       if (file.ObjectId && (file.ObjectId & 0x7F000000) == 0x6000000 && file.FileSize == 4120) {
         console.log("yes", { file: file });
-        let r = new SeekableFileReader(portal_path, file.FileOffset);
 
-        r.ReadUint32();
-        r.ReadUint32();
+        let file_reader = new SeekableFileReader(portal_path, file.FileOffset);
+        let icon = new PortalIcon();
+        icon.unpack(file_reader);
 
-        let form = r.ReadUint32();
-        console.log(form);
-        if (form == 10 || form == 6) {
-          let width = r.ReadUint32();
-          let height = r.ReadUint32();
-          let format = r.ReadUint32();
-          let bufLen = r.ReadUint32();
-
-          console.log({ width, height, format, bufLen });
-          let buf = Buffer.alloc(bufLen);
-
-          for (let i = 0; i < bufLen; i++) {
-            buf[i] = r.ReadUint8()
+        // WIP: Export
+        sharp(icon.buffer, {
+          raw: {
+            width: icon.width || 0,
+            height: icon.height || 0,
+            channels: 4
           }
-
-          console.log(buf)
-          sharp(buf, {
-            raw: {
-              width: width,
-              height: height,
-              channels: 4
-            }
-          }).png().toFile("out.png");
-
-          console.log("ten");
-          break;
-        }
-
-        // ByteCursor csr = library.LoadFile(file);
-        // int nForm = csr.GetDWORD(true);
-        // if (nForm == 10 || nForm == 6) {
-        //   images[i] = (nForm == 10) ? csr.GetRGBImage(true) : csr.Get24BitImage(true);
-        //   imgIndex = iconList.Images.Count;
-        //   iconList.Images.Add(images[i]);
-        // }
-
-        break;
+        }).png().toFile("latest.png");
       }
     }
   }
---
+++ acdatservice/src/portal_icon.ts
@@ -0,0 +1,35 @@
+import SeekableFileReader from "./seekable_file_reader";
+
+export class PortalIcon {
+  width: number | undefined
+  height: number | undefined
+  format: number | undefined
+  length: number | undefined
+  buffer: Buffer | undefined
+
+  unpack(reader: SeekableFileReader) {
+    // TODO: What is this?
+    reader.ReadUint32();
+    // TODO: What is this?
+    reader.ReadUint32();
+
+    // Form? What is form?
+    let form = reader.ReadUint32();
+
+    console.log(form);
+
+    if (form == 10 || form == 6) {
+      this.width = reader.ReadUint32();
+      this.height = reader.ReadUint32();
+      this.format = reader.ReadUint32();
+      this.length = reader.ReadUint32();
+    } else {
+      throw new Error("TODO")
+    }
+
+    this.buffer = Buffer.alloc(this.length);
+    for (let i = 0; i < this.length; i++) {
+      this.buffer[i] = reader.ReadUint8()
+    }
+  }
+}

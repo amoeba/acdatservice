3c6c20f752fcb57f310cb78214b190662c71fcbc
Bryce Mecum <petridish@gmail.com>
2025-06-05 20:26:17 -0700

    Finish basic indexing

--- src/bin/create_index.rs
+++ src/bin/create_index.rs
@@ -1,5 +1,10 @@
+use libac_rs::dat::reader::dat_database::DatDatabase;
 use sqlite::{self, Connection};
-use std::fs;
+use std::{
+    env,
+    fs::{self, File},
+    path::Path,
+};
 
 fn setup() -> Result<(), Box<dyn std::error::Error>> {
     fs::create_dir_all("./data")?;
@@ -14,33 +19,47 @@ fn migrate(connection: &Connection) -> Result<(), Box<dyn std::error::Error>> {
             id INTEGER,
             type TEXT NOT NULL,
             subtype TEXT,
-            offset INTEGER NOT NULL
+            offset INTEGER NOT NULL,
+            extra_info JSON
         )",
     )?;
 
     Ok(())
 }
 
-fn insert_test_data(connection: &Connection) -> Result<(), Box<dyn std::error::Error>> {
-    let test_data = [
-        (0, "texture", "icon", 0),
-        (0, "texture", "icon", 1024),
-        (0, "texture", "icon", 2048),
-        (0, "texture", "icon", 4096),
-        (0, "texture", "icon", 8192),
-        (0, "texture", "icon", 16384),
-        (0, "texture", "icon", 32768),
-        (0, "texture", "icon", 65536),
-    ];
+fn show_data(connection: &Connection) -> Result<(), Box<dyn std::error::Error>> {
+    let mut statement = connection.prepare("SELECT count(1) FROM files;")?;
 
-    for (id, file_type, subtype, offset) in test_data {
+    while let sqlite::State::Row = statement.next()? {
+        let count: i64 = statement.read(0)?;
+        println!("Count: {}", count);
+    }
+
+    Ok(())
+}
+
+fn create_index(connection: &Connection, dat_path: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let mut db_file = File::open(dat_path)?;
+    let db: DatDatabase = DatDatabase::read(&mut db_file)?;
+    let files = db.list_files(true)?;
+
+    for file in files {
+        println!("Processing file: {:?}", file);
+
+        let dat_file_type = file.file_type();
+
+        // TODO: Eventually handle more types
+        if dat_file_type != libac_rs::dat::enums::dat_file_type::DatFileType::Texture {
+            continue;
+        }
+
         let mut statement = connection
             .prepare("INSERT INTO files (id, type, subtype, offset) VALUES (?, ?, ?, ?)")?;
 
-        statement.bind((1, id))?;
-        statement.bind((2, file_type))?;
-        statement.bind((3, subtype))?;
-        statement.bind((4, offset as i64))?;
+        statement.bind((1, file.object_id as i64))?;
+        statement.bind((2, dat_file_type.to_string().as_str()))?;
+        statement.bind((3, "subtype"))?;
+        statement.bind((4, file.file_offset as i64))?;
 
         statement.next()?;
     }
@@ -48,31 +67,28 @@ fn insert_test_data(connection: &Connection) -> Result<(), Box<dyn std::error::E
     Ok(())
 }
 
-fn show_data(connection: &Connection) -> Result<(), Box<dyn std::error::Error>> {
-    let mut statement = connection.prepare("SELECT * FROM files")?;
-    while let sqlite::State::Row = statement.next()? {
-        let id: i64 = statement.read(0)?;
-        let file_type: String = statement.read(1)?;
-        let subtype: Option<String> = statement.read(2)?;
-        let offset: i64 = statement.read(3)?;
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<String> = env::args().collect();
 
-        println!(
-            "ID: {}, Type: {}, Subtype: {:?}, Offset: {}",
-            id, file_type, subtype, offset
-        );
+    if args.len() != 2 {
+        return Err(Box::from("Must specify path to dat file to index."));
     }
 
-    Ok(())
-}
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let dat_path = &args[1];
+
+    if !Path::new(dat_path).exists() {
+        return Err(Box::from(format!(
+            "Provided dat file path doesn't exist: {}",
+            dat_path
+        )));
+    }
+
     let db_path = "./data/index.sqlite";
     let connection = sqlite::open(db_path)?;
 
     setup()?;
     migrate(&connection)?;
-
-    // TODO: Replace with real data
-    insert_test_data(&connection)?;
+    create_index(&connection, dat_path)?;
     show_data(&connection)?;
 
     Ok(())

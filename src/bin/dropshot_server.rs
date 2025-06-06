1431e46b6510210fd171b7a47be6a2a5d7dfb4a4
Bryce Mecum <petridish@gmail.com>
2025-06-05 21:11:48 -0700

    Move to dedicate bin, hook up sqlite

--- src/bin/dropshot_server.rs
+++ src/bin/dropshot_server.rs
@@ -51,6 +51,67 @@ async fn myapi_dats_get_dat_file(
     Ok(HttpResponseOk(dat_file))
 }
 
+#[derive(Serialize, JsonSchema)]
+struct FileEntry {
+    id: i64,
+    file_type: String,
+    subtype: String,
+    offset: i64,
+}
+
+#[derive(Serialize, JsonSchema)]
+struct FilesResponse {
+    files: Vec<FileEntry>,
+}
+
+fn list_files() -> Result<Vec<FileEntry>, Box<dyn std::error::Error>> {
+    let db_path = "./data/index.sqlite";
+    let connection = sqlite::open(db_path)?;
+    let mut statement =
+        connection.prepare("SELECT id, type, subtype, offset FROM files LIMIT 1024;")?;
+
+    let mut files: Vec<FileEntry> = Vec::new();
+
+    while let sqlite::State::Row = statement.next()? {
+        let id: i64 = statement.read(0)?;
+        let file_type: String = statement.read(1)?;
+        let file_subtype: String = statement.read(2)?;
+        let offset: i64 = statement.read(3)?;
+
+        files.push(FileEntry {
+            id: id,
+            file_type: file_type,
+            subtype: file_subtype,
+            offset: offset,
+        });
+    }
+
+    Ok(files)
+}
+
+#[endpoint(
+    method = GET,
+    path = "/files",
+)]
+async fn myapi_files_index(
+    _rqctx: RequestContext<()>,
+) -> Result<HttpResponseOk<FilesResponse>, HttpError> {
+    // VERY WIP
+    // TODO: Connect once? Maybe no need since this is fast.
+
+    match list_files() {
+        Ok(files) => {
+            let response = FilesResponse { files };
+            Ok(HttpResponseOk(response))
+        }
+        Err(e) => {
+            return Err(HttpError::for_internal_error(
+                format!("Failed to execute list files query: {:?}", e).to_string(),
+            ))
+        }
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), String> {
     let log = ConfigLogging::StderrTerminal {
@@ -68,6 +129,7 @@ async fn main() -> Result<(), String> {
 
     api.register(myapi_dats_get).unwrap();
     api.register(myapi_dats_get_dat_file).unwrap();
+    api.register(myapi_files_index).unwrap();
 
     let server = ServerBuilder::new(api, (), log)
         .config(config_dropshot)
